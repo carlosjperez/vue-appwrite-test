8afdfd60cede5881db26710ac921ccb0c08f8074
divanov11 <ivanovsin11@gmail.com>
2024-10-20 14:20:30 -0700

    Updated client config

--- src/appwrite.js
+++ src/appwrite.js
@@ -3,8 +3,8 @@ import { Client, Account } from "appwrite";
 const { VITE_ENDPOINT, VITE_PROJECT_ID } = import.meta.env;
 
 const client = new Client()
-    .setEndpoint(VITE_ENDPOINT)
-    .setProject(VITE_PROJECT_ID);
+    .setEndpoint(VITE_ENDPOINT || "")
+    .setProject(VITE_PROJECT_ID || "");
 
 const account = new Account(client);
 
